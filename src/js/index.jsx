e9ec6548eaf816ebfdcd000d3f22585340cc39cb
sgrund14 <sgrund@oberlin.edu>
2017-08-20 14:31:53 -0400

    maybe make app fullscreen

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,13 +1,5 @@
 'use strict';
 
-// maybe this hides the address bar
-window.addEventListener("load", function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
-
 const React = require('react');
 const ReactDOM = require('react-dom');
 require('../scss/main.scss');
