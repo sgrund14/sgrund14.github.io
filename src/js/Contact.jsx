d1664d5fab48fc4a0371ea8e855de7e4f2280068
sgrund14 <sgrund@oberlin.edu>
2018-07-25 20:00:33 -0400

    new effect on work hover (

--- src/js/Contact.jsx
+++ src/js/Contact.jsx
@@ -10,12 +10,12 @@ const Contact = ({ hideAll, onContact, onSettings }) => {
 				<div className='panel upper contact'>
 					<a className="btn media-link" id="github-btn" href='https://github.com/sgrund14' target='_blank'>github</a>
 					<a className="btn media-link" id="arena-btn" href='https://www.are.na/samuel-grund/channels' target='_blank'>are.na</a>
-					<p>twitter</p>
+					<a href="https://www.twitter.com/gruno14" className="btn media-link" target="_blank">twitter</a>
 				</div>
 			</div>
 			<div className='bottom-panel bottom-color'>
 				<div className='panel lower contact'>
-					<p>instagram</p>
+					<a href="https://www.instagram.com/gruno14" className="btn media-link" target="_blank">instagram</a>
 					<a className="btn media-link" id="behance-btn" href='https://www.behance.net/sgrund' target='_blank'>behance</a>
 					<a className="btn media-link" id="linkedin-btn" href='https://www.linkedin.com/in/samuel-grund-2a7481108' target='_blank'>linkedin</a>
 				</div>
